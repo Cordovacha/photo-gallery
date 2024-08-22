d4d532d4bb451c22a1b1bc3998829b42261d7216
Cordovacha <supremodota@gmail.com>
2024-08-21 20:57:42 -0400

    Proyect-Ionic-Android

---
+++ src/router/index.ts
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from "@ionic/vue-router";
+import { RouteRecordRaw } from "vue-router";
+import TabsPage from "../views/TabsPage.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    redirect: "/tabs/tab2",
+  },
+  {
+    path: "/tabs/",
+    component: TabsPage,
+    children: [
+      {
+        path: "",
+        redirect: "/tabs/tab2",
+      },
+
+      {
+        path: "tab2",
+        component: () => import("@/views/Tab2Page.vue"),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
